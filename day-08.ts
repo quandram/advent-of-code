294d1a613e99f8bacb7952788f6fcb0316ee5fb0
quandram <quandram@users.noreply.github.com>
2024-01-14 19:50:47 +0000

    Day 8 - part 2 - wip

--- day-08.ts
+++ day-08.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 
-const debug = true;
+const debug = false;
 const fileName = `day-08${debug ? ".test" : ".input"}`;
 
 const data: Array<string> = readFileSync(fileName, "utf8")
@@ -21,15 +21,16 @@ data.forEach((x, i) => {
 
 function runMap() {
     let i: number = 0;
+    const instructions1 = instructions;
     let location = 'AAA';
     while (location !== "ZZZ") {
-        const nextInstruction = instructions.shift();
+        const nextInstruction = instructions1.shift();
         if (typeof(nextInstruction) === "undefined") {
             return;
         }
         location = getDestination(location, nextInstruction);
         i++;
-        instructions.push(nextInstruction);
+        instructions1.push(nextInstruction);
         
     }
     return i;
@@ -40,3 +41,36 @@ function getDestination(mapIndex: string, instruction: number) {
 }
 
 console.log(`Part 1: ${runMap()} steps`);
+
+// while not all is Ending node
+// run next loop
+//
+
+function part2() {
+    let i: number = 0;
+    const instructions2 = instructions;
+    console.log("a");
+    let currentNodes = Object.keys(map).filter(x => x.slice(-1) === 'A');
+    const endingNodes = Object.keys(map).filter(x => x.slice(-1) === "Z");
+    currentNodes = currentNodes.filter((x, i) => i === 3);
+    while(currentNodes.some(x => endingNodes.indexOf(x) === -1) && i < 4000000) {
+        console.log("-----");
+        console.log(i);
+        console.log(currentNodes);
+        const nextInstruction = instructions2.shift();
+        if (typeof(nextInstruction) === "undefined") {
+            return;
+        }
+        console.log("-----");
+        currentNodes = currentNodes.map(x => getDestination(x, nextInstruction));
+        console.log("-----");
+        i++;
+        instructions2.push(nextInstruction);
+        if (i === 20777) {
+            console.log(currentNodes);
+            console.log(endingNodes);
+        }
+    }
+    return i;
+}
+console.log(`Part 2: ${part2()}`);
